ed4d62f0aecf3575e6a55303005bca8cb51e75c4
Jackson Kurtz <jkurtz678@gmail.com>
2022-06-05 19:01:07 -0700

    New Deployment

--- assets/types.8335cbb0.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./firebaseConfig.d7dbf4c4.js";class s{static createBaseEntity(){const t=a.now();return{created_at:t,updated_at:t}}static updateBaseEntity(){return{updated_at:a.now()}}}export{s as B};
