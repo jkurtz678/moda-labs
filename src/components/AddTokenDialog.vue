e400fbff7c1c88513d24a8d0c5aceb5bb7344bce
Grace <grace.l@onestepgps.com>
2022-06-14 20:33:14 -0700

    dialog update

--- src/components/AddTokenDialog.vue
+++ src/components/AddTokenDialog.vue
@@ -5,14 +5,16 @@
       :fullscreen="screen_type == 'xs'" custom-class="add-dialog">
       <el-card class="box-card" shadow="never">
         <div v-if="sort_token_metas.length == 0">No tokens found</div>
-        <AddTokenItem :plaque_id="plaque_id" :token="i" v-for="i in sort_token_metas"
-          @update_token_list="update_token_list" :submit_new_token="submit_new_token" :clear="ref_new_token_meta_id"></AddTokenItem>
+        <AddTokenItem v-for="token in sort_token_metas" :token="token" :in_list="token_in_list_map[token.id]"
+          @update_token_list="updateLocalTokenList"></AddTokenItem>
         <hr class="hr" />
       </el-card>
       <template #footer>
         <span class="dialog-footer">
+          <div>{{ `Tokens in playlist: ${new_token_meta_id_list.length}` }}</div>
+          <div style="flex-grow: 1"></div>
           <el-button @click="clearList">Clear</el-button>
-          <el-button type="info" @click="updateList">Done</el-button>
+          <el-button type="info" @click="updateList" :loading="save_loading">Save</el-button>
         </span>
       </template>
     </el-dialog>
@@ -20,12 +22,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from "vue";
-import { getTokenMetas } from "@/api/token-meta";
+import { computed, ref, watch } from "vue";
 import { updatePlaque } from "@/api/plaque"
 import { usePlaqueStore } from "@/stores/plaque"
 import AddTokenItem from './AddTokenItem.vue';
-import { ElLoading, ElMessage } from 'element-plus'
+import { showError } from "@/util/util"
 import type { FirestoreDocument, TokenMeta, Plaque } from "@/types/types";
 import useBreakpoints from "@/composables/breakpoints"
 import { useTokenMetaStore } from "@/stores/token-meta";
@@ -33,11 +34,13 @@ import { useTokenMetaStore } from "@/stores/token-meta";
 interface AddTokenDialogProps {
   show_add_token_dialog: boolean;
   plaque_id: string;
-  submit_new_token: boolean;
-  clear:string[]
 }
 
 const props = defineProps<AddTokenDialogProps>();
+const new_token_meta_id_list = ref<string[]>([]);
+const save_loading = ref(false);
+
+
 const emit = defineEmits(['update:show_add_token_dialog',])
 const { width, screen_type } = useBreakpoints();
 const plaque_store = usePlaqueStore();
@@ -50,7 +53,12 @@ const show_dialog = computed({
     emit('update:show_add_token_dialog', value)
   }
 })
-const submit_new_token = ref(false)
+watch(show_dialog, (v) => {
+  if (v) {
+    new_token_meta_id_list.value = JSON.parse(JSON.stringify(plaque.value.entity.token_meta_id_list));
+  }
+})
+
 const token_metas = computed(() => {
   return token_meta_store.archive_token_meta_list;
 })
@@ -72,22 +80,35 @@ const sort_token_metas = computed(() => {
 const plaque = computed((): FirestoreDocument<Plaque> => {
   return plaque_store.plaque_map[props.plaque_id];
 })
-const new_token_meta_id: string[] = JSON.parse(JSON.stringify(plaque.value.entity.token_meta_id_list))
-const ref_new_token_meta_id = ref(new_token_meta_id)
-const update_token_list = (new_id: string) => {
-  if (ref_new_token_meta_id.value.some(id => id === new_id)) {
-    ref_new_token_meta_id.value = ref_new_token_meta_id.value.filter(id => id != new_id);
+
+interface InListMap {
+  [id: string]: boolean;
+}
+const token_in_list_map = computed((): InListMap => {
+  const in_list_map: InListMap = {}
+  new_token_meta_id_list.value.forEach((t) => {
+    in_list_map[t] = true;
+  })
+  return in_list_map;
+});
+
+const updateLocalTokenList = (new_id: string) => {
+  if (new_token_meta_id_list.value.some(id => id === new_id)) {
+    new_token_meta_id_list.value = new_token_meta_id_list.value.filter(id => id != new_id);
   } else {
-    ref_new_token_meta_id.value.push(new_id);
+    new_token_meta_id_list.value.push(new_id);
   }
 }
 const updateList = async () => {
-  await updatePlaque(props.plaque_id, { token_meta_id_list: ref_new_token_meta_id.value });
-  submit_new_token.value = true
+  save_loading.value = true;
+  await updatePlaque(props.plaque_id, { token_meta_id_list: new_token_meta_id_list.value }).catch(err => {
+    showError(`Error updating plaque tokens - ${err}`)
+  })
+  save_loading.value = false;
   show_dialog.value = false
 }
 const clearList = () => {
-  ref_new_token_meta_id.value = []
+  new_token_meta_id_list.value = []
 }
 
 </script>
@@ -113,5 +134,11 @@ const clearList = () => {
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
   width: 50%;
   height: 100%;
+}
+
+.dialog-footer {
+  display: flex;
+  align-items: center;
+  padding: 0px 5px 0px 5px;
 }
 </style> 
--- src/components/AddTokenItem.vue
+++ src/components/AddTokenItem.vue
@@ -8,8 +8,7 @@
             <p class="bold">{{ props.token.entity.name }}</p>
             <p class="bold">{{ props.token.entity.artist }}</p>
         </div>
-        <el-button :icon="getIcon" :type="getType" plain circle @click="toggleTokenMeta"
-            :loading="toggle_token_loading" />
+        <el-button :icon="in_list ? 'close' : 'plus'" :type="in_list ? 'danger' : 'success'" plain circle @click=" emit('update_token_list', props.token.id)" />
     </div>
 
 </template>
@@ -24,71 +23,17 @@ import { defaultMaxListeners } from "events";
 import { pipelineTopicExpression } from "@babel/types";
 
 interface AddTokenItemProps {
+    in_list: boolean;
     token: FirestoreDocument<TokenMeta>;
-    plaque_id: string;
-    submit_new_token: boolean;
-    clear: string[];
 }
 const props = defineProps<AddTokenItemProps>();
-const toggle_token_loading = ref(false);
-const new_tmp_list: string[] = [];
-const new_list = ref(new_tmp_list);
-const in_playlist = computed(() => {
-    console.log("in_playlist", plaque_store.meta_in_playlist(props.plaque_id, props.token.id))
 
-    return plaque_store.meta_in_playlist(props.plaque_id, props.token.id)
-})
-const in_newlist = computed(() => {
-    return new_list.value.some(id => id == props.token.id);
-})
-const plaque = computed((): FirestoreDocument<Plaque> => {
-    return plaque_store.plaque_map[props.plaque_id];
-})
-const reset = ref(props.submit_new_token)
-const plaque_store = usePlaqueStore();
 const emit = defineEmits(['update_token_list'])
-const toggleTokenMeta = () => {
-    if (reset.value) {
-        new_list.value = []
-        reset.value = false
-    }
-    if (!in_newlist.value) {
-        new_list.value.push(props.token.id)
-    } else {
-        new_list.value = new_list.value.filter(id => id != props.token.id)
-    }
-    emit('update_token_list', props.token.id)
-};
 
 const getImageUrl = (filename: string) => {
     return new URL(`../assets/${filename}`, import.meta.url).href
-}
-const getType = computed(() => {
-    // if(props.clear.length!=0){
-    //     console.log("here",props.clear)
-    // }
+} 
 
-    if (in_playlist.value && !in_newlist.value) {
-        return 'danger'
-    } else if (in_playlist.value && in_newlist.value) {
-        return 'success'
-    } else if (!in_playlist.value && in_newlist.value) {
-        return 'danger'
-    } else {
-        return 'success'
-    }
-})
-const getIcon = computed(() => {
-    if (in_playlist.value && !in_newlist.value) {
-        return 'close'
-    } else if (in_playlist.value && in_newlist.value) {
-        return 'plus'
-    } else if (!in_playlist.value && in_newlist.value) {
-        return 'close'
-    } else {
-        return 'plus'
-    }
-})
 // watch(add_to_list, async (v) => {
 //     if (v) {
 //         if (in_playlist.value) {
