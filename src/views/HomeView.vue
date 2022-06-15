9393340f30a6d6403857948db525da6f18e962c5
Jackson Kurtz <jkurtz678@gmail.com>
2022-06-15 00:26:23 -0700

    add plaque from query param

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,18 +11,20 @@
 <script setup lang="ts">
 import Header from "@/components/Header.vue";
 import { onMounted, ref } from "vue"
-import { useRouter, RouterView } from 'vue-router';
+import { useRouter, useRoute, RouterView } from 'vue-router';
 import { useAccountStore } from "@/stores/account"
 import { usePlaqueStore } from "@/stores/plaque"
 import { useTokenMetaStore } from "@/stores/token-meta"
 import { ElLoading, ElMessage } from 'element-plus'
 import { showError } from "@/util/util";
+import { updatePlaque } from "@/api/plaque";
 
 const account_store = useAccountStore();
 const plaque_store = usePlaqueStore();
 const token_meta_store = useTokenMetaStore();
 const router = useRouter();
 const initial_load_done = ref(false);
+const route = useRoute()
 
 onMounted(async () => {
   const loading = ElLoading.service({
@@ -33,7 +35,7 @@ onMounted(async () => {
   const { wallet_address, signature, ens_name } = account_store.getCachedAccountData()
   // if account address is not found in local storage, redirect to landing. ens_name can be empty
   if (!wallet_address || !signature) {
-    router.push({ name: "landing" });
+    router.push({ name: "landing", query: { redir: window.location.href } });
     loading.close()
     return
   }
@@ -41,7 +43,7 @@ onMounted(async () => {
   // load account first
   await account_store.loadAccount(wallet_address, signature, ens_name)
   if (account_store.account == null) {
-    ElMessage("Error - failed to load account")
+    showError("Error - failed to load account")
     return
   }
 
@@ -52,8 +54,15 @@ onMounted(async () => {
     .catch(err => (showError(`Error loading archive token metas - ${err}`)));
   const opensea_token_promise = token_meta_store.loadOpenseaTokenMetas(account_store.get_account.entity.wallet_address)
     .catch(err => (showError(`Error loading opensea tokens - ${err}`)))
-
   await Promise.all([plaque_promise, archive_token_promise, opensea_token_promise])
+
+  // if plaque_id is passed in the query params and the plaque is not in the store, attempt to add this plaque to this account
+  const plaque_id_qp = route.query.plaque_id;
+  if (plaque_id_qp && typeof plaque_id_qp === 'string' && !plaque_store.plaque_map[plaque_id_qp]) {
+    await updatePlaque(plaque_id_qp, { wallet_address: account_store.get_account.entity.wallet_address, token_meta_id_list: [] }).catch(err => {
+      showError(`Error adding plaque from query param = ${err}`)
+    })
+  }
   initial_load_done.value = true;
   loading.close()
 });
