ef068b7a603e57a8d00190529d604e48e34c009b
Grace <grace.l@onestepgps.com>
2022-06-17 00:15:21 -0700

    fix card layout

--- src/components/PlaqueTokenItem.vue
+++ src/components/PlaqueTokenItem.vue
@@ -18,13 +18,21 @@
   </div>
   <div class="card-body" v-if="isExpand.expanded">
     <div class="card-flex-container">
-      <div style="flex:1; width:30%">
-        <div>{{ props.token_meta.entity.name }}</div>
-        <img style="width:121px" :src="getImageUrl('logo.png')" />
+      <div class="card-flex-left">
+        <div class="flex-column">
+          <p class="card-title">Title</p>
+          <div class="bold bigger-font">{{ props.token_meta.entity.name }}</div>
+          <img style="width:80px" :src="getImageUrl('logo.png')" />
+        </div>
       </div>
-      <div style="flex: 3 1 0%; width:70%">
+      <div class="card-flex-right">
+        <p class="card-title">Artist name</p>
         <div class="bold bigger-font description">{{ props.token_meta.entity.artist }}</div>
-        <div class="description">{{ props.token_meta.entity.description }}</div>
+        <div :class="show_all ? 'description-all' : 'description-less'">{{ props.token_meta.entity.description }}</div>
+        <a @click="show_all = !show_all" class="link">
+          <span v-if="show_all">Less</span>
+          <span v-else> More</span>
+        </a>
       </div>
     </div>
   </div>
@@ -57,15 +65,48 @@ const thumbnail_image = computed(() => {
 const getImageUrl = (filename: string) => {
   return new URL(`../assets/${filename}`, import.meta.url).href
 }
+const show_all = ref(false);
 </script>
 
 <style>
-.description {
-  padding: 0 1em;
+.card-title {
+  line-height: 0.5em;
+}
+
+.link:hover {
+  color: hotpink;
+  cursor: pointer;
+}
+
+.link {
+  text-decoration: underline;
+}
+
+.link span {
+  font-weight: bold;
+}
+
+.description-less {
+  /* padding: 0 1em; */
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+  max-width: 400px;
+}
+
+.description-all {
+  /* padding: 0 1em; */
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+  max-width: 400px;
 }
 
 .bigger-font {
-  font-size: 1.5em;
+  font-size: 1.2em;
 }
 
 .card-flex-container {
@@ -80,12 +121,28 @@ const getImageUrl = (filename: string) => {
 }
 
 .card-body {
-  max-width: 404px;
+  max-width: 600px;
 }
 
 .bold {
   font-weight: 700;
 }
 
+.card-flex-left {
+  width: 25%;
+  margin: 5px;
+  /* height: 100vh; */
+  padding: 5px;
+}
 
+.card-flex-right {
+  width: 75%;
+  /* height: 50vh; */
+  margin: 5px;
+  padding: 5px;
+}
+
+.flex-column {
+  flex-direction: column;
+}
 </style>
