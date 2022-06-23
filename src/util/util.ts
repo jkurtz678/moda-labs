50cd697df2fb3ad9ed241398dbfbc428d8409c54
Jackson Kurtz <jkurtz678@gmail.com>
2022-06-23 13:18:32 -0400

    add caroline to admin

--- src/util/util.ts
+++ src/util/util.ts
@@ -16,6 +16,7 @@ export function getAdminWalletAddressList(): string[] {
         "0xd8945d98ed4233Cf87cfA4fDCC7a54FE279E8ee7", // nathan
         "0x2ee434a49C4C5A254669d05E1602c656017bAC1f", // ryan
         "0x0599B2ddEF49E3Ef6b11a5969cBB9aDE7fA614dd", // genevieve?
+        "0xEa53074F2972F2EE9C00410b127e107F29c7D4E3" // caroline
     ];
 
     return admin_wallet_address_list
