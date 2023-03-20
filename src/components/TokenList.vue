d2a68a56f469e54b9019f749d6deedd5820da93e
Jackson Kurtz <jkurtz678@gmail.com>
2023-03-19 23:58:41 -0700

    add sorting logic to token list

--- src/components/TokenList.vue
+++ src/components/TokenList.vue
@@ -6,8 +6,13 @@
                     <span style="font-size: 18px;">Artwork</span>
                     <el-button icon="Plus" @click="router.push({ name: 'new-token' })" color="#000000">Add artwork to archive</el-button>
                 </div>
-                <el-input v-model="search_filter" :prefix-icon="Search" style="margin-top: 20px;"
-                    placeholder="Search by artwork title or artist name"></el-input>
+                <div style="display: flex; align-items: center; margin-top: 20px;">
+                    <el-input v-model="search_filter" :prefix-icon="Search" placeholder="Search by artwork title or artist name"></el-input>
+                    <el-select v-model="sort_order" placeholder="Sort by" style="margin-left: 10px;">
+                        <el-option label="Sort by name" value="name"></el-option>
+                        <el-option label="Sort by newest" value="date_submitted"></el-option>
+                    </el-select>
+                </div>
             </template>
             <div v-if="tokens.length == 0">No artwork found</div>
             <PlaqueTokenItem v-for="token in tokens" :token_meta="token" >
@@ -27,12 +32,21 @@ const router = useRouter();
 
 const token_meta_store = useTokenMetaStore()
 const search_filter = ref("")
+const sort_order = ref("name")
 
 const tokens = computed(() => {
     const store_tokens = token_meta_store.sorted_all_token_metas;
-    return store_tokens.filter((token) =>
+    const filtered_tokens = store_tokens.filter((token) =>
         token.entity.artist?.toLowerCase().includes(search_filter.value.toLowerCase()) || token.entity.name?.toLowerCase().includes(search_filter.value.toLowerCase())
     );
+
+    if (sort_order.value === "name") {
+        return filtered_tokens.sort((a, b) => a.entity.name.localeCompare(b.entity.name));
+    } else if (sort_order.value === "date_submitted") {
+        return filtered_tokens.sort((a, b) => b.entity.created_at.seconds - a.entity.created_at.seconds);
+    }
+
+    return [];
 })
 
 </script>
