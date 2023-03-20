98554724a80b74aeaca685154dbc8d89bfc9866f
Jackson Kurtz <jkurtz678@gmail.com>
2023-03-20 00:12:46 -0700

    fix sort button errors

--- src/components/TokenList.vue
+++ src/components/TokenList.vue
@@ -4,18 +4,20 @@
             <template #header>
                 <div class="card-header">
                     <span style="font-size: 18px;">Artwork</span>
-                    <el-button icon="Plus" @click="router.push({ name: 'new-token' })" color="#000000">Add artwork to archive</el-button>
+                    <el-button icon="Plus" @click="router.push({ name: 'new-token' })" color="#000000">Add artwork to
+                        archive</el-button>
                 </div>
                 <div style="display: flex; align-items: center; margin-top: 20px;">
-                    <el-input v-model="search_filter" :prefix-icon="Search" placeholder="Search by artwork title or artist name"></el-input>
+                    <el-input v-model="search_filter" :prefix-icon="Search"
+                        placeholder="Search by artwork title or artist name"></el-input>
                     <el-select v-model="sort_order" placeholder="Sort by" style="margin-left: 10px;">
                         <el-option label="Sort by name" value="name"></el-option>
-                        <el-option label="Sort by newest" value="date_submitted"></el-option>
+                        <el-option label="Sort by newest" value="created_at"></el-option>
                     </el-select>
                 </div>
             </template>
             <div v-if="tokens.length == 0">No artwork found</div>
-            <PlaqueTokenItem v-for="token in tokens" :token_meta="token" >
+            <PlaqueTokenItem v-for="token in tokens" :token_meta="token">
             </PlaqueTokenItem>
         </el-card>
     </div>
@@ -42,8 +44,23 @@ const tokens = computed(() => {
 
     if (sort_order.value === "name") {
         return filtered_tokens.sort((a, b) => a.entity.name.localeCompare(b.entity.name));
-    } else if (sort_order.value === "date_submitted") {
-        return filtered_tokens.sort((a, b) => b.entity.created_at.seconds - a.entity.created_at.seconds);
+    } else if (sort_order.value === "created_at") {
+        return filtered_tokens.sort((a, b) => {
+            // if both art null return 0
+            if (a.entity.created_at == null && b.entity.created_at == null) {
+                return 0;
+            }
+
+            // then if one is null it should go last
+            if (a.entity.created_at == null) {
+                return 1;
+            }
+            if (b.entity.created_at == null) {
+                return -1;
+            }
+
+            return b.entity.created_at.seconds - a.entity.created_at.seconds
+        });
     }
 
     return [];
