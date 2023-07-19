2cf94578125de86f5ad01cfac9733cec21b55859
Jackson Kurtz <jkurtz678@gmail.com>
2023-07-19 02:07:14 -0700

    add artwork tile button

--- src/components/ArtworkTile.vue
+++ src/components/ArtworkTile.vue
@@ -11,19 +11,35 @@
 
     <div class='detail-container transition' :class="show_detail ? 'show-blur' : 'hide-blur absolute'">
       <div style="font-size: 1.6em; font-weight: bold;">{{ token_meta.entity.name }}</div>
-      <div style="font-weight: bold;">{{ token_meta.entity.artist}}</div>
-      <div style="font-size: 0.9em; line-height: 1.3em">{{ token_meta.entity.description}}</div>
+      <div style="font-weight: bold;">{{ token_meta.entity.artist }}</div>
+      <template v-if="show_detail">
+      <el-tooltip class="box-item" effect="dark" content="Download art" placement="top">
+        <el-button icon="Download" text circle @click="openArt"></el-button>
+      </el-tooltip>
+      <el-tooltip class="box-item" effect="dark" content="Preview plaque" placement="top">
+        <el-button icon="Tickets" text circle @click="previewPlaque"></el-button>
+      </el-tooltip>
+      <el-tooltip class="box-item" effect="dark" content="Edit art data" placement="top">
+        <el-button icon="Edit" text circle
+          @click="router.push({ name: 'edit-artwork', params: { 'token_meta_id': props.token_meta.id } })">
+        </el-button>
+      </el-tooltip>
+      </template>
+      <div style="font-size: 0.9em; line-height: 1.3em">{{ token_meta.entity.description }}</div>
     </div>
   </div>
 </template>
   
 <script setup lang="ts">
 import useThumbnail from '@/composables/thumbnail-image';
-import { getPlatformDisplay, type FirestoreDocument, type TokenMeta } from '@/types/types';
+import { getPlatformDisplay, type FirestoreDocument, type TokenMeta, getSourceFile } from '@/types/types';
+import { showError } from '@/util/util';
 import { ref } from 'vue';
 import { computed } from 'vue';
 import { toRef } from 'vue';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 interface ArtworkTileProps {
   token_meta: FirestoreDocument<TokenMeta>;
 }
@@ -34,6 +50,23 @@ const show_detail = ref(false);
 const platform = computed(() => {
   return getPlatformDisplay(props.token_meta.entity.platform)
 })
+
+const openArt = async () => {
+  const url = await getSourceFile(props.token_meta);
+
+  if (!url) {
+    showError("Error getting source file")
+    return
+  }
+
+  window.open(url, '_blank');
+}
+
+
+const previewPlaque = () => {
+  const link = router.resolve({ name: 'preview-plaque', params: { token_meta_id: props.token_meta.id } });
+  window.open(link.href);
+}
 
 
 </script>
@@ -46,7 +79,6 @@ const platform = computed(() => {
   background: #F5F5F5;
   box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.5);
   transition: box-shadow 0.6s;
-  transition: height 0.5s;
 }
 
 .custom-card:hover {
@@ -79,12 +111,12 @@ const platform = computed(() => {
 }
 
 .custom-card:hover .platform {
-  opacity: 90%;
+  opacity: 85%;
 }
 
 
 .custom-card:hover .header {
-  opacity: 90%;
+  opacity: 85%;
 }
 
 img {
@@ -96,7 +128,7 @@ img {
 }
 
 .show-blur {
-  opacity: 0.9 !important;
+  opacity: 0.85 !important;
 }
 
 .hide-blur {
@@ -109,9 +141,10 @@ img {
 
 
 .detail-container {
-  background-color: white; 
+  background-color: white;
   opacity: 0;
   padding: 1em;
+  min-height: 279px;
 }
 
 .absolute {
