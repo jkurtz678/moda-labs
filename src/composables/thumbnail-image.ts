4b8b71a8ab05a99ffddafb1330157454f339a627
Jackson Kurtz <jkurtz678@gmail.com>
2023-08-04 12:10:10 -0700

    medium files working generation

--- src/composables/thumbnail-image.ts
+++ src/composables/thumbnail-image.ts
@@ -1,16 +1,16 @@
 import { watchEffect, ref, type Ref } from "vue"
-import { getTokenMetaThumbnailImageURL, type FirestoreDocument, type TokenMeta } from "@/types/types"
+import { getTokenMetaMediumImageURL, type FirestoreDocument, type TokenMeta } from "@/types/types"
 
 // use Ref to make token_meta param stay reactive
 export default function useThumbnail(token_meta: Ref<FirestoreDocument<TokenMeta>> ) {
     const thumbnail_url = ref(new URL(`../assets/logo.png`, import.meta.url).href);
     // needs to be watchEffect because we want it to trigger initially AND we want it to run again when the props.token_meta changes
     watchEffect(async () => {
-        thumbnail_url.value = await getTokenMetaThumbnailImageURL(token_meta.value);
+        thumbnail_url.value = await getTokenMetaMediumImageURL(token_meta.value);
         // if firebase archive thumbnail is not found, try again in 5 seconds
         if (thumbnail_url.value.includes("logo.png")) {
             setTimeout(async () => {
-                thumbnail_url.value = await getTokenMetaThumbnailImageURL(token_meta.value);
+                thumbnail_url.value = await getTokenMetaMediumImageURL(token_meta.value);
             }, 12000);
         }
     });
